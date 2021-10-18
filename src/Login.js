fbdc6ca9ba4e45a9c1010a5a80f3e4c4610fffca
tonyngeno <tony.ngeno@systechafrica.com>
2021-10-18 20:16:43 +0300

    add css format to login page component

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,25 @@ import React from 'react'
 import './Login.css'
 
 function Login() {
+    const loginToApp  = () => {};
+    const register  = () => {};
     return (
        <div className="login">
-           <h1>You are not Logged in</h1>
+         <img src="https://1000logos.net/wp-content/uploads/2017/03/Linkedin-Logo.png" alt="" />
+         <form >
+             
+             <input type="text" placeholder = "Full Name (Required if registering)" />
+             <input type="text" placeholder = "Profile Picture URL (optional)" />
+             <input type="email" placeholder = "Email" />
+             <input type="password" placeholder = "Password" />
+             <button type = 'submit' onClick = {loginToApp}>Sign In</button>
+
+         </form>
+         <p>Not A Member ?
+             <span className= 'login_register' onClick = {register}>
+                 Register Now
+             </span>
+         </p>
        </div>
     )
 }
