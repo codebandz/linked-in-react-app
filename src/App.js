4894113db0fa29329990e7a7aa505f339c0dd359
tonyngeno <tony.ngeno@systechafrica.com>
2021-10-18 13:53:59 +0300

    add sidebar css and content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
@@ -7,12 +8,9 @@ function App() {
   return (
     <div className="app">
      <Header/>
-     {/*App Body*/}
      <div className = 'app_body'>
        <Sidebar />
-    
-     {/*Sidebar*/}
-
+       <Feed/>
      {/*Feed*/}
      
      {/*Widgets*/ }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,14 @@ import React from 'react'
 import './Sidebar.css'
 
 function Sidebar() {
+
+    const recentItem = (topic) => (
+        <div className="sidebar_recentItem">
+            <span className = 'sidebar_hash'>#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
     return (
         <div className = 'sidebar'>
             <div className="sidebar_top">
@@ -24,10 +32,16 @@ function Sidebar() {
                         2,070
                     </p>
                 </div>
+            </div>
                 <div className="sidebar_bottom">
                     <p>Recent</p>
+                    {recentItem('reactJs')}
+                    {recentItem('programming')}
+                    {recentItem('software engineering')}
+                    {recentItem('software design')}
+                    {recentItem('developer')}
+                    {recentItem('code')}
                 </div>
-            </div>
         </div>
     )
 }
