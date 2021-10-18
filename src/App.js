9ad1a84ebfff494af3230f04f914466f971c5ed0
tonyngeno <tony.ngeno@systechafrica.com>
2021-10-18 11:04:41 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-     <h1>Lets Build A Linkedin Clone</h1>
+
+     {/*Header*/}
+     <Header/>
+     {/*App Body*/}
+
+     {/*Sidebar*/}
+
+     {/*Sidebar*/}
+     
+     {/*Widgets*/}
+
     </div>
   );
 }
